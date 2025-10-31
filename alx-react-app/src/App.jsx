4cbb6ec78791fae2d2d37e77d9198a5acb0eb5ab
nmwaura4 <nmwaura4@gmail.com>
2025-10-31 19:32:47 +0300

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,22 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+
 function App() {
   return (
     <div>
+      <h1>User Profile Example</h1>
       <WelcomeMessage />
+      <UserProfile name="John Doe" age={30} bio="Avid traveler and photographer." />
       <Header />
       <MainContent />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
       <Footer />
     </div>
   );
