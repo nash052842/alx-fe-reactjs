60d4a2c19fb8a12049af7a05484fdc318f0a58d7
nmwaura4 <nmwaura4@gmail.com>
2025-11-08 18:42:20 +0300

    initial commit

--- alx-react-app-new/src/UserContext.js
+++
@@ -1,5 +0,0 @@
-// src/UserContext.js
-import { createContext } from 'react';
-
-const UserContext = createContext(null); // Default value can be null
-export default UserContext;
--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px 0' }}>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
-};
-
-export default Footer;
