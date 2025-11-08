2d7175be6fde15e2ed8611a66344950b49662370
nmwaura4 <nmwaura4@gmail.com>
2025-11-08 16:03:22 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+<footer>
+  <style>
+    footer {
+      background-color: #333;
+      color: white;
+      text-align: center;
+      padding: 10px 0;
+    }
+  </style>
+  <p>© 2023 City Lovers</p>
+</footer>
+
+export default Footer;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+<main>
+  <style>
+    main {
+      padding: 20px;
+      font-family: Arial, sans-serif;
+      backgroundColor: '#a19393ff';
+        }
+  </style>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+export default MainContent;
+  
