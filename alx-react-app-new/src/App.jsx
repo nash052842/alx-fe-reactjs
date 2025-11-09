bf888218a950c42459d20c042a298bc1123f4b74
nmwaura4 <nmwaura4@gmail.com>
2025-11-09 20:09:17 +0300

    initial

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <MainContent />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
