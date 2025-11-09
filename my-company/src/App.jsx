3d1944dd728bf60377270e35bc8b57f5451527f6
nmwaura4 <nmwaura4@gmail.com>
2025-11-09 19:36:05 +0300

    Navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
 function App() {
   return (
