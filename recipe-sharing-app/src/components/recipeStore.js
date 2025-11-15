0179d1353835f7fa4ae1038034921198661740d8
nmwaura4 <nmwaura4@gmail.com>
2025-11-15 17:38:11 +0300

    recipestore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -19,3 +19,34 @@ export const useRecipeStore = create((set) => ({
   })),
   setRecipes: (recipes) => set({ recipes }),
 }));
+
+
+import create from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  // State
+  recipes: [],
+
+  // Actions
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
+}));
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
