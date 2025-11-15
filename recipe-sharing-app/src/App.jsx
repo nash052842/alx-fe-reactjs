1621a941299743365c7a9548f5550de7594d4fdf
nmwaura4 <nmwaura4@gmail.com>
2025-11-15 16:46:54 +0300

    app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
+import AddRecipeForm from '../../components/AddRecipeForm';
+import RecipeList from '../../components/RecipeList';
 
 function App() {
   const [count, setCount] = useState(0);
