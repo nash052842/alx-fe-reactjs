eca2447c9171bfbbc70636bb3bf784223013228e
nmwaura4 <nmwaura4@gmail.com>
2025-11-22 15:28:21 +0300

    initial commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-import Search from "./components/Search";
-=======
+
 import React from "react";
 import SearchBar from "./components/SearchBar";
 >>>>>>> 8ff9448 (initial commit)
@@ -9,11 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>GitHub User Search</h1>
-<<<<<<< HEAD
-      <Search />
-=======
       <SearchBar />
->>>>>>> 8ff9448 (initial commit)
     </div>
   );
 }
