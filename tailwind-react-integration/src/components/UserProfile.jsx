2bd3859d7372f443613efc32c4d127952b2ea78f
nmwaura4 <nmwaura4@gmail.com>
2025-11-26 20:01:53 +0300

    UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,16 +1,18 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm 
+                    mx-auto my-10 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
       {/* Profile Image */}
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto 
+                   hover:scale-110 transition-transform duration-300 ease-in-out"
       />
 
-
       {/* User Name */}
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-2 sm:my-4 text-center">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-2 sm:my-4 text-center 
+                     hover:text-blue-500 transition-colors duration-300">
         John Doe
       </h1>
 
