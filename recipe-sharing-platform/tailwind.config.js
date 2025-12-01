da4aad6cf7e94fe90d21955147b0daeafcc783b0
nmwaura4 <nmwaura4@gmail.com>
2025-12-01 18:32:27 +0300

    initial commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
-/module.exports = {
+module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // You can set it to 'media' or 'class' if needed
+  darkMode: false, // 'media' or 'class'
   theme: {
     extend: {},
   },
