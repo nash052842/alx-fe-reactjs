41ef0ee015d38dc289bc0b9978bf231d6594e399
nmwaura4 <nmwaura4@gmail.com>
2025-12-12 15:15:01 +0300

    good

---
+++ vite-project/src/formikForm.jsx
@@ -0,0 +1,48 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email format").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+function FormikForm() {
+  return (
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        console.log(values);
+        alert("Registration Successful with Formik!");
+        resetForm();
+      }}
+    >
+      {() => (
+        <Form>
+          <div>
+            <label>Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" style={{ color: "red" }} />
+          </div>
+
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default FormikForm;
