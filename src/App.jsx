bfa3a566ff51a6b46bc5da3dd881db909b41dd83
nmwaura4 <nmwaura4@gmail.com>
2025-12-12 12:13:00 +0300

    Initial commit after reinit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostsComponent from "./components/PostsComponent.jsx";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
