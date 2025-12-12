f881bd3948cef34c7b4918c248b7ea07fcde0b18
nmwaura4 <nmwaura4@gmail.com>
2025-12-12 12:55:09 +0300

    react -route-demo

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostsComponent from "./components/PostsComponent.jsx";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Query Advanced Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
