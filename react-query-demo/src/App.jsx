3fd07c83cdbf6e99e0328a200967ac2b120f162a
nmwaura4 <nmwaura4@gmail.com>
2025-12-12 13:04:54 +0300

    react-router

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent.jsx";
+
+// Create a QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>React Query Advanced Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: "20px" }}>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
